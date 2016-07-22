24091e4d2339d375ca11a9a8e0ede17eb561ae5f
csboyty <csboyty@gmail.com>
2016-07-22 14:52:58 +0800

    update

--- staticWeb/js/src/config.js
+++ staticWeb/js/src/config.js
@@ -14,9 +14,10 @@ var config={
         swfUrl:"resources/js/lib/Moxie.swf"
     },
     ajaxUrls:{
-        brandGetAll:"http://192.168.2.8:8080/SDX_HPGL/",
-        brandAdd:"add.php",
-        brandDelete:"add.php"
+        brandGetAll:"http://192.168.2.8:8080/SDX_HPGL/brand/getData",
+        brandAdd:"http://192.168.2.8:8080/SDX_HPGL/brand/insert",
+        brandUpdate:"http://192.168.2.8:8080/SDX_HPGL/brand/update",
+        brandDelete:"http://192.168.2.8:8080/SDX_HPGL/brand/delete"
     },
     dataTable:{
         langUrl:"lang/de_DE.txt"
