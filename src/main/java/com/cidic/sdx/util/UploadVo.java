1faf59bcaf2e9011371f1367e4173b0327dd8613
codeliling <maximuslee@126.com>
2016-07-29 17:23:58 +0800

    add upload code

--- src/main/java/com/cidic/sdx/util/UploadVo.java
+++ src/main/java/com/cidic/sdx/util/UploadVo.java
@@ -18,14 +18,14 @@ public class UploadVo {
         this.imgFile = imgFile;
     }
  
-    @Override
+    @Override 
     public String toString() {
         return "UploadVo [imgFile=" + imgFile + "]";
     }
-     
+
     public boolean validateFile() throws SdxException{
-        if(!this.getImgFile().getContentType().equals("xls") || !this.getImgFile().getContentType().equals("xmls")){
-        	throw new SdxException(500, "文件类型只能是xls、xmls！");
+        if(!this.getImgFile().getContentType().equals("xls") || !this.getImgFile().getContentType().equals("xlsx ")){
+        	throw new SdxException(500, "文件类型只能是xls、xlsx！");
         }
         if(this.getImgFile().getSize() > 1000 * 10000){
             throw new SdxException(500,"文件最大不能超过10M！");
